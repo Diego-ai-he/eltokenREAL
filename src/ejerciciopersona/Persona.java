7f9cd450445cf15bf1a8476cc214380659e3fc4f
vina <vina@VIAWSE02LC0426.duoc.acad>
2024-08-26 21:35:11 -0400

    primer programa

---
+++ src/ejerciciopersona/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciopersona;
+
+/**
+ *
+ * @author vina
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+            
+}
